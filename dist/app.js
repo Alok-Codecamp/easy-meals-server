77cdd95e9620a23e5f1c42f5c31afabcd8666566
Alok-Codecamp <alok61.bd@gmail.com>
2025-04-10 14:46:17 +0600

    cors origin change

--- dist/app.js
+++ dist/app.js
@@ -13,7 +13,7 @@ const providers_routes_1 = require("./app/modules/mealProviders/providers.routes
 const orders_routes_1 = require("./app/modules/orders/orders.routes");
 const app = (0, express_1.default)();
 app.use(express_1.default.json());
-app.use((0, cors_1.default)({ origin: 'http://localhost:5173', credentials: true }));
+app.use((0, cors_1.default)({ origin: 'http://localhost:3000', credentials: true }));
 app.use((0, cookie_parser_1.default)());
 app.get('/', (req, res) => {
     res.json('EasyMeals server is live and running smoothly!');
